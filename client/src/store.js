49952d683959998f4ceca5db3965837d1436f992
jfkeci <ja.lip132@gmail.com>
2021-10-14 16:58:05 +0200

    chore: warehouse

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import Auth from './warehouse/Auth'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+    modules: {
+        Auth
+    },
     state: {
 
     },
