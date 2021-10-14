7aa98d6c7d8f551d9f58b32d66b8370b0116fc79
jfkeci <ja.lip132@gmail.com>
2021-10-14 16:43:10 +0200

    chore: login and register form structure

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,46 +1,59 @@
 <template>
   <div>
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form-group
-        id="input-group-1"
-        label="Email address:"
-        label-for="input-1"
-        description="We'll never share your email with anyone else."
-      >
-        <b-form-input
-          id="input-1"
-          v-model="form.email"
-          type="email"
-          placeholder="Enter email"
-          required
-        ></b-form-input>
-      </b-form-group>
+    <b-card class="mt-3" header="Login">
+      <b-form @submit="onSubmit" class="mt-3">
+        <b-form-group
+          id="input-group-1"
+          label="Your Email:"
+          label-for="input-1"
+        >
+          <b-form-input
+            id="input-1"
+            v-model="form.email"
+            type="email"
+            placeholder="Email"
+            required
+          ></b-form-input>
+        </b-form-group>
 
-      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="form.password"
-          placeholder="Enter name"
-          required
-        ></b-form-input>
-      </b-form-group>
+        <b-form-group
+          id="input-group-2"
+          label="Your Password"
+          label-for="input-2"
+        >
+          <b-form-input
+            id="input-2"
+            v-model="form.password"
+            placeholder="Password"
+            type="password"
+            required
+          ></b-form-input>
+        </b-form-group>
 
-      <b-button type="submit" variant="primary">Login</b-button>
-    </b-form>
+        <b-button type="submit" class="mt-3" variant="primary">Login</b-button>
+        &nbsp;&nbsp;&nbsp;
+        <router-link to="/register">Need an account?</router-link>
+      </b-form>
+    </b-card>
+    <b-card class="mt-3" header="Form">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
   </div>
 </template>
 
 <script>
 export default {
   name: "Login",
-  form: {
-    email: "",
-    password: "",
+  data() {
+    return {
+      form: {
+        email: "",
+        password: "",
+      },
+    };
   },
-  watch: {
-    form() {
-      console.log(this.form);
-    },
+  methods: {
+    onSubmit() {},
   },
 };
 </script>
---
+++ client/src/views/Register.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <b-card class="mt-3" header="Login">
+      <b-form @submit="onSubmit" class="mt-3">
+        <b-form-group id="input-group-1" label="Your Name:" label-for="input-1">
+          <b-form-input
+            id="input-1"
+            v-model="form.name"
+            type="text"
+            placeholder="Name"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-2"
+          label="Your Username:"
+          label-for="input-2"
+        >
+          <b-form-input
+            id="input-2"
+            v-model="form.username"
+            type="text"
+            placeholder="Username"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-3"
+          label="Your Email:"
+          label-for="input-3"
+        >
+          <b-form-input
+            id="input-3"
+            v-model="form.email"
+            type="email"
+            placeholder="Email"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-4"
+          label="Your Password"
+          label-for="input-4"
+        >
+          <b-form-input
+            id="input-4"
+            v-model="form.password"
+            placeholder="Password"
+            type="password"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-5"
+          label="Confirm password"
+          label-for="input-5"
+        >
+          <b-form-input
+            id="input-5"
+            v-model="form.confirm_password"
+            placeholder="Confirm password"
+            type="password"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-button type="submit" class="mt-3" variant="primary">Login</b-button>
+      </b-form>
+    </b-card>
+    <b-card class="mt-3" header="Form">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      form: {
+        name: "",
+        username: "",
+        email: "",
+        password: "",
+        confirm_password: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit() {},
+  },
+};
+</script>
+
+<style scoped>
+</style>
