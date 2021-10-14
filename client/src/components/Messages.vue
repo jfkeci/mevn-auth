34cef0b843524c79a9d9e5ef9fb65bf5dde46406
jfkeci <ja.lip132@gmail.com>
2021-10-14 16:46:04 +0200

    feat: messages component

---
+++ client/src/components/Messages.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-alert
+      v-for="(message, key) in messages"
+      :key="key"
+      class="m-1"
+      :variant="message.type"
+      show
+      >{{ message.text }}</b-alert
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Messages",
+  data() {
+    return {
+      messages: [],
+    };
+  },
+};
+</script>
