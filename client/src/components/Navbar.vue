d24dcf51a01f6525c66bb905d703085c6bfab39e
jfkeci <ja.lip132@gmail.com>
2021-10-14 15:41:27 +0200

    feat: client side vue app with router

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="/">Mevn auth</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/about">About</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item to="/login">Login</b-dropdown-item>
+            <b-dropdown-item to="/register">Register</b-dropdown-item>
+            <b-dropdown-item to="/profile">Profile</b-dropdown-item>
+            <b-dropdown-item to="/logout">Logout</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+<style>
+</style>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          placeholder="Enter email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.password"
+          placeholder="Enter name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Login</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  form: {
+    email: "",
+    password: "",
+  },
+  watch: {
+    form() {
+      console.log(this.form);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
