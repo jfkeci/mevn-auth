257b95a30517db1a9c0b1a012569045bba6885d6
jfkeci <ja.lip132@gmail.com>
2021-10-14 18:12:25 +0200

    feat: login with email or username

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -10,14 +10,14 @@
       <b-form @submit.prevent="onSubmit" class="mt-3">
         <b-form-group
           id="input-group-1"
-          label="Your Email:"
+          label="Your Email or Username:"
           label-for="input-1"
         >
           <b-form-input
             id="input-1"
-            v-model="user.email"
-            type="email"
-            placeholder="Email"
+            v-model="input"
+            type="text"
+            placeholder="Email or Username"
             required
           ></b-form-input>
         </b-form-group>
@@ -44,9 +44,6 @@
         <router-link to="/register">Need an account?</router-link>
       </b-form>
     </b-card>
-    <b-card class="mt-3" header="User">
-      <pre class="m-0">{{ user }}</pre>
-    </b-card>
   </div>
 </template>
 
@@ -58,21 +55,20 @@ export default {
   mixins: [messagesMixin],
   data() {
     return {
-      user: {
-        email: "",
-        password: "",
-      },
+      input: "",
+      user: { password: "" },
     };
   },
   methods: {
     ...mapActions(["login"]),
     onSubmit() {
+      this.setUserObject();
       this.login(this.user)
         .then((res) => {
+          console.log(res);
           if (res.data.success) {
             this.$router.push({ name: "Profile" });
           } else {
-            console.log(res.data.message);
             this.setMessage("danger", res.data.message);
           }
         })
@@ -80,6 +76,14 @@ export default {
           console.log(err);
           this.setMessage("danger", "Something went wrong");
         });
+    },
+    setUserObject() {
+      this.user = { password: this.user.password };
+      if (this.input.includes("@")) {
+        this.user["email"] = this.input;
+      } else {
+        this.user["username"] = this.input;
+      }
     },
   },
 };
