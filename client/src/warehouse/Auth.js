7622ddc3695375849198f845df45a0eac666e8ca
jfkeci <ja.lip132@gmail.com>
2021-10-14 17:07:07 +0200

    update: state and getters in Auth warehouse

--- client/src/warehouse/Auth.js
+++ client/src/warehouse/Auth.js
@@ -1,12 +1,24 @@
 /* import axios from 'axios';
 import router from '../router' */
 
+// Basic initial state of the application
 const state = {
-
+    token: localStorage.getItem('token') || '',
+    user: {},
+    status: ''
 }
 
 const getters = {
-
+    /* isLoggedIn: (state) => {
+        if (state.token != '') {
+            return true
+        } else {
+            return false
+        }
+    } */
+    isLoggedIn: state => !!state.token,
+    authState: state => state.status,
+    user: state => state.user
 }
 
 const actions = {
